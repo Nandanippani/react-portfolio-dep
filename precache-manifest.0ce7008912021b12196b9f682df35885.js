bfc5ad15109e6be2e9a81293f430dfbacf14e26c
Nanda Nippani <nandanippani@yahoo.com>
2023-02-08 22:32:19 +0000

    Updates

---
+++ precache-manifest.0ce7008912021b12196b9f682df35885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87aa4ed9439657afdd14",
+    "url": "/react-portfolio-dep/static/css/main.918c0125.chunk.css"
+  },
+  {
+    "revision": "87aa4ed9439657afdd14",
+    "url": "/react-portfolio-dep/static/js/main.87aa4ed9.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/react-portfolio-dep/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "c04b887d16035eac5507",
+    "url": "/react-portfolio-dep/static/js/2.c04b887d.chunk.js"
+  },
+  {
+    "revision": "57cb011f1e0a97abc5f1",
+    "url": "/react-portfolio-dep/static/js/runtime~main.57cb011f.js"
+  },
+  {
+    "revision": "698aaaf458200f2493afece0ced15c52",
+    "url": "/react-portfolio-dep/index.html"
+  }
+];
